8a02d8537cd6c0c770a93a3d0ae4c6d55da110a1
imilano <m15752836560_1@163.com>
2021-05-23 09:29:10 +0800

    Modify ci workflow

--- dgs/room_service_test.go
+++ dgs/room_service_test.go
@@ -1,25 +1,41 @@
 package dgs
 
+import (
+	"happyball-matcher/configs"
+	"os"
+	"testing"
+)
+
 func initProgram() {
 	InitConnection("localhost:9000")
 }
 
-//func TestCreateRoom(t *testing.T) {
-//	initProgram()
-//	sa := InitConnection(configs.DgsAddr)
-//	roomId, err := sa.CreateRoom()
-//	if err != nil {
-//		t.Error("出错了:", err.Error())
-//	}
-//	t.Log("roomId为", roomId)
-//}
-//
-//func TestGetRoomList(t *testing.T) {
-//	initProgram()
-//	sa :=  InitConnection(configs.DgsAddr)
-//	roomList, err := sa.GetRoomList()
-//	if err != nil {
-//		t.Error("出错了:", err.Error())
-//	}
-//	t.Logf("roomList为%+v", roomList)
-//}
+func TestCreateRoom(t *testing.T) {
+	skipCI(t)
+	initProgram()
+	sa := InitConnection(configs.DgsAddr)
+	roomId, err := sa.CreateRoom()
+	if err != nil {
+		t.Error("出错了:", err.Error())
+	}
+	t.Log("roomId为", roomId)
+}
+
+func TestGetRoomList(t *testing.T) {
+	skipCI(t)
+	initProgram()
+	sa :=  InitConnection(configs.DgsAddr)
+	roomList, err := sa.GetRoomList()
+	if err != nil {
+		t.Error("出错了:", err.Error())
+	}
+	t.Logf("roomList为%+v", roomList)
+}
+
+
+
+func skipCI(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping testing in CI environment")
+	}
+}
